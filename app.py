4bd7e75cd16571200e2ea5c5e944979c8de2b46f
Darren McPherson <dmcp@macbook-pro.home>
2020-03-15 16:02:29 +0000

    adding proper linter for flask sqlalchemy. Creating a product class witha  constructor

--- app.py
+++ app.py
@@ -12,5 +12,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+class Product(db.Model):
+    id = db.Column(db.Integar, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integar)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
 if __name__ == '__main__':
     app.run(debug=True)
