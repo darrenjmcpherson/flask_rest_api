427b7062cacb864c8b013ce089cfba0b84187cdf
Darren McPherson <dmcp@macbook-pro.home>
2020-03-15 16:07:57 +0000

    cant spell integer apparently

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 class Product(db.Model):
-    id = db.Column(db.Integar, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
-    qty = db.Column(db.Integar)
+    qty = db.Column(db.Integer)
 
     def __init__(self, name, description, price, qty):
         self.name = name
