8988fa0623449afd321309bedee1e7094811139f
Darren McPherson <dmcp@macbook-pro.home>
2020-03-15 15:57:12 +0000

    connect sqlA to app and marshmallow to app

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
